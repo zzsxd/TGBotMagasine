7007bdde1bbc4e05ec65228227649aee69fdf265
lilfuz <lilfuz@gmail.com>
2024-04-01 20:36:59 +0300

    I win

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ from backend import TempUserData, DbAct
 from db import DB
 
 
-
 def main():
     @bot.message_handler(commands=['start', 'admin'])
     def start_msg(message):
@@ -39,30 +38,162 @@ def main():
 
     @bot.callback_query_handler(func=lambda call: True)
     def callback(call):
+        user_id = call.message.chat.id
         buttons = Bot_inline_btns()
-        # пользовательские команды
-        if call.data == 'assortiment':
-            bot.send_message(call.message.chat.id, 'Наш ассортимент товаров: ', reply_markup=buttons.assortiment_btns())
-        elif call.data == 'cart':
-            bot.send_message(call.message.chat.id, 'Корзина')
-        elif call.data == 'bonus':
-            bot.send_message(call.message.chat.id, 'Наши скидки и акции', reply_markup=buttons.bonus_btns())
-        elif call.data == 'reviews':
-            bot.send_message(call.message.chat.id, 'Отзывы', reply_markup=buttons.reviews_btns())
-        elif call.data == 'faq':
-            bot.send_message(call.message.chat.id, 'Ответы на все вопросы', reply_markup=buttons.faq_btns())
-        elif call.data == ''
-        # команды админа
-        elif call.data == 'export':
-            db_actions.db_export_xlsx()
-            bot.send_document(call.message.chat.id, open(config.get_config()['xlsx_path'], 'rb'))
-            os.remove(config.get_config()['xlsx_path'])
-        elif call.data == 'addtovar':
-            pass #добавление товара
-        elif call.data == 'changetovar':
-            pass #изменение товара
-        elif call.data == 'newsletter':
-            pass #рассылка всем пользователям
+        if db_actions.user_is_existed(user_id):
+            print(1)
+            code = temp_user_data.temp_data(user_id)[user_id][0]
+            if call.data == 'assortiment':
+                temp_user_data.temp_data(user_id)[user_id][0] = 10
+                categories = db_actions.get_categories()
+                bot.send_message(user_id, 'Выберите категорию: ',
+                                 reply_markup=buttons.assortiment_btns(categories))
+            elif call.data == 'cart':
+                s = ''
+                shipping_cart = db_actions.get_shipping_cart_by_user_id(user_id)
+                for i in range(len(shipping_cart)):
+                    s += f'{i+1}. {db_actions.get_product_by_id(shipping_cart[i])[0]}\n'
+                bot.send_message(user_id, f'Ваша корзина:\n{s}', reply_markup=buttons.pay_shipping_cart())
+            elif call.data == 'bonus':
+                bot.send_message(call.message.chat.id, 'Наши скидки и акции', reply_markup=buttons.bonus_btns())
+            elif call.data == 'pay_shipping_cart':
+                bot.send_message(call.message.chat.id, 'Кода нет - тимлид уснул')
+            elif call.data == 'reviews':
+                bot.send_message(call.message.chat.id, 'Отзывы', reply_markup=buttons.reviews_btns())
+            elif call.data == 'faq':
+                bot.send_message(call.message.chat.id, 'Ответы на все вопросы', reply_markup=buttons.faq_btns())
+            elif call.data[:8] == 'category' and code == 10:
+                temp_user_data.temp_data(user_id)[user_id][0] = None
+                products = db_actions.products_by_id_category(call.data[8:])
+                for product in products:
+                    bot.send_photo(chat_id=user_id, caption=f'{product[1]}\n\n{product[2]}', photo=product[3],
+                                   reply_markup=buttons.add_product_to_shipping_cart(product[0]))
+            elif call.data[:8] == 'addtobuy':
+                if db_actions.update_shipping_cart(user_id, call.data[8:]):
+                    bot.answer_callback_query(call.id, "Товар добавлен в корзину", show_alert=True)
+                else:
+                    bot.answer_callback_query(call.id, "Товар уже в корзине", show_alert=True)
+            if db_actions.user_is_admin(user_id):
+                if call.data == 'export':
+                    db_actions.db_export_xlsx()
+                    bot.send_document(call.message.chat.id, open(config.get_config()['xlsx_path'], 'rb'))
+                    os.remove(config.get_config()['xlsx_path'])
+                elif call.data[:8] == 'category' and code == 3:
+                    temp_user_data.temp_data(user_id)[user_id][1][3] = call.data[8:]
+                    db_actions.add_product(temp_user_data.temp_data(user_id)[user_id][1])
+                    temp_user_data.temp_data(user_id)[user_id][0] = None
+                    bot.send_message(user_id, 'Товар успешно добавлен!')
+                elif call.data[:8] == 'category' and code == 5:
+                    db_actions.del_categories(call.data[8:])
+                    temp_user_data.temp_data(user_id)[user_id][0] = None
+                    bot.send_message(user_id, 'Категория удалена успешно!')
+                elif call.data[:8] == 'category' and code == 9:
+                    db_actions.update_product('categori_id', call.data[8:], temp_user_data.temp_data(user_id)[user_id][2])
+                    temp_user_data.temp_data(user_id)[user_id][0] = None
+                    bot.send_message(user_id, 'Товар успешно обновлён!')
+                elif call.data == 'delcategory':
+                    temp_user_data.temp_data(user_id)[user_id][0] = 5
+                    categories = db_actions.get_categories()
+                    bot.send_message(user_id, 'Выберите категорию для удаления',
+                                     reply_markup=buttons.categories_btns(categories))
+                elif call.data[:22] == 'finally_change_product':
+                    match call.data[22]:
+                        case '1':
+                            temp_user_data.temp_data(user_id)[user_id][0] = 6
+                            bot.send_message(user_id, 'Введите новое название')
+                        case '2':
+                            temp_user_data.temp_data(user_id)[user_id][0] = 7
+                            bot.send_message(user_id, 'Введите новое описание')
+                        case '3':
+                            temp_user_data.temp_data(user_id)[user_id][0] = 8
+                            bot.send_message(user_id, 'Отправьте новую обложку')
+                        case '4':
+                            categories = db_actions.get_categories()
+                            temp_user_data.temp_data(user_id)[user_id][0] = 9
+                            bot.send_message(user_id, 'Выберите новую категорию', reply_markup=buttons.categories_btns(categories))
+                elif call.data[:14] == 'change_product':
+                    temp_user_data.temp_data(user_id)[user_id][2] = call.data[14:]
+                    bot.send_message(user_id, 'Что вы хотите изменить?',
+                                     reply_markup=buttons.change_peoduct_btns())
+                elif call.data == 'addtovar':
+                    temp_user_data.temp_data(user_id)[user_id][0] = 0
+                    bot.send_message(user_id, 'Введите название товара')
+                elif call.data == 'addcategory':
+                    temp_user_data.temp_data(user_id)[user_id][0] = 4
+                    bot.send_message(user_id, 'Введите название категории')
+                elif call.data == 'changetovar':
+                    products = db_actions.get_products_preview()
+                    bot.send_message(user_id, 'Выберите товар',
+                                     reply_markup=buttons.product_btns(products))
+                elif call.data == 'newsletter':
+                    pass #рассылка всем пользователям
+
+    @bot.message_handler(content_types=['text', 'photo'])
+    def text_message(message):
+        photo = message.photo
+        user_input = message.text
+        user_id = message.chat.id
+        buttons = Bot_inline_btns()
+        if db_actions.user_is_existed(user_id):
+            code = temp_user_data.temp_data(user_id)[user_id][0]
+            match code:
+                case 0:
+                    if user_input is not None:
+                        temp_user_data.temp_data(user_id)[user_id][1][0] = user_input
+                        temp_user_data.temp_data(user_id)[user_id][0] = 1
+                        bot.send_message(user_id, 'Отправьте фото товара')
+                    else:
+                        bot.send_message(user_id, 'Это не текст!')
+                case 1:
+                    if photo is not None:
+                        photo_id = photo[-1].file_id
+                        photo_file = bot.get_file(photo_id)
+                        photo_bytes = bot.download_file(photo_file.file_path)
+                        temp_user_data.temp_data(user_id)[user_id][1][1] = photo_bytes
+                        temp_user_data.temp_data(user_id)[user_id][0] = 2
+                        bot.send_message(user_id, 'Отправьте описание товара')
+                    else:
+                        bot.send_message(user_id, 'Это не фото!')
+                case 2:
+                    if user_input is not None:
+                        temp_user_data.temp_data(user_id)[user_id][1][2] = user_input
+                        temp_user_data.temp_data(user_id)[user_id][0] = 3
+                        categories = db_actions.get_categories()
+                        bot.send_message(user_id, 'Выберите категорию для товара', reply_markup=buttons.categories_btns(categories))
+                    else:
+                        bot.send_message(user_id, 'Это не текст!')
+                case 4:
+                    if user_input is not None:
+                        db_actions.add_category(user_input)
+                        temp_user_data.temp_data(user_id)[user_id][0] = None
+                        bot.send_message(user_id, 'Категория успешно добавлена!')
+                    else:
+                        bot.send_message(user_id, 'Это не текст!')
+                case 6:
+                    if user_input is not None:
+                        db_actions.update_product('name', user_input, temp_user_data.temp_data(user_id)[user_id][2])
+                        temp_user_data.temp_data(user_id)[user_id][0] = None
+                        bot.send_message(user_id, 'Товар успешно обновлён!')
+                    else:
+                        bot.send_message(user_id, 'Это не текст!')
+                case 7:
+                    if user_input is not None:
+                        db_actions.update_product('description', user_input, temp_user_data.temp_data(user_id)[user_id][2])
+                        temp_user_data.temp_data(user_id)[user_id][0] = None
+                        bot.send_message(user_id, 'Товар успешно обновлён!')
+                    else:
+                        bot.send_message(user_id, 'Это не текст!')
+                case 8:
+                    if photo is not None:
+                        photo_id = photo[-1].file_id
+                        photo_file = bot.get_file(photo_id)
+                        photo_bytes = bot.download_file(photo_file.file_path)
+                        db_actions.update_product('photo', photo_bytes, temp_user_data.temp_data(user_id)[user_id][2])
+                        temp_user_data.temp_data(user_id)[user_id][0] = None
+                        bot.send_message(user_id, 'Товар успешно обновлён!')
+                    else:
+                        bot.send_message(user_id, 'Это не фото!')
+
     bot.polling(none_stop=True)
 
 
